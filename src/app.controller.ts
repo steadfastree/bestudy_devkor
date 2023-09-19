9437e340d556021ed8eaadd0b8fecb843554acb5
Jeonghwan Park <pjhisgod@naver.com>
2023-09-20 01:11:32 +0900

    feat: get users, create user, delete user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,60 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Param,
+  Req,
+  Res,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
 @Controller('/users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUsers(@Res() res: Response) {
+    // 모든 유저 가져오기
+    try {
+      const users = this.appService.getUsers();
+      res.status(200).send(users); //OK
+    } catch (error) {
+      res.status(400).send(error.messages);
+    }
   }
-  //parameter와 query로 url을 통해 데이터 전달.
-  //get /users/:id -> parameter,
-  @Get('/:index')
+
+  @Get('/:index') //index 위치의 유저 가져오기
   getUsernameByIndex(@Param('index') index: number, @Res() res: Response) {
     try {
       const username = this.appService.getUsernameByIndex(index);
-      res.send(username);
+      res.status(200).send(username); //OK
+    } catch (error) {
+      res.status(400).send(error.messages);
+    }
+  }
+
+  @Post('/:index') //index 위치에 새 유저 추가, usernames.length 이상을 지정하면 맨 뒤에 추가.
+  createUser(
+    @Param('index') index: number,
+    @Body('username') username: string,
+    @Res() res: Response,
+  ) {
+    try {
+      this.appService.createUser(index, username);
+      res.status(201).send('created successfully'); // created successfully
+    } catch (error) {
+      res.status(400).send(error.messages);
+    }
+  }
+
+  @Delete('/:index') // index 위치의 유저 삭제하기
+  deleteUserByIndex(@Param('index') index: number, @Res() res: Response) {
+    try {
+      this.appService.deleteUser(index);
+      res.status(204).send('deleted successfully'); //deleted successfully
     } catch (error) {
       res.status(400).send(error.messages);
     }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,17 +4,31 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   usernames: string[];
   constructor() {
-    this.usernames = ['a', 'b'];
+    this.usernames = ['Dominic', 'Jeff', 'Peter'];
   }
 
-  getHello(): string {
-    return 'Hello World!';
+  getUsers() {
+    return this.usernames;
   }
 
   getUsernameByIndex(index: number) {
     if (index < 0 || index >= this.usernames.length) {
       throw Error('Index out of range');
     }
-    return this.usernames[index];
+    return this.usernames[index]; // 인덱스 받아서 범위 안에 있으면 해당 유저네임 출력.
+  }
+
+  createUser(index: number, username: string) {
+    if (username.length <= 0) {
+      throw Error('Invalid Username');
+    }
+    this.usernames.splice(index, 0, username);
+  }
+
+  deleteUser(index: number) {
+    if (index < 0 || index >= this.usernames.length) {
+      throw Error('Index out of range');
+    }
+    this.usernames.splice(index, 1);
   }
 }
