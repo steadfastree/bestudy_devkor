5926e52b58b2285670c5b441f05b02808f7cf683
Jeonghwan Park <pjhisgod@naver.com>
2023-09-21 20:55:33 +0900

    fix : built-in exceptions, post res

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,9 +20,10 @@ export class AppController {
     // 모든 유저 가져오기
     try {
       const users = this.appService.getUsers();
-      res.status(200).send(users); //OK
+      res.status(200).json(users); //OK
     } catch (error) {
-      res.status(400).send(error.messages);
+      console.error('ERROR : ', error.message);
+      res.status(error.status).json(error.message);
     }
   }
 
@@ -30,9 +31,10 @@ export class AppController {
   getUsernameByIndex(@Param('index') index: number, @Res() res: Response) {
     try {
       const username = this.appService.getUsernameByIndex(index);
-      res.status(200).send(username); //OK
+      res.status(200).json(username); //OK
     } catch (error) {
-      res.status(400).send(error.messages);
+      console.error('ERROR : ', error.message);
+      res.status(error.status).json(error.message);
     }
   }
 
@@ -43,20 +45,21 @@ export class AppController {
     @Res() res: Response,
   ) {
     try {
-      this.appService.createUser(index, username);
-      res.status(201).send('created successfully'); // created successfully
+      res.status(201).json(this.appService.createUser(index, username)); // created successfully
     } catch (error) {
-      res.status(400).send(error.messages);
+      console.error('ERROR : ', error.message);
+      res.status(error.status).json(error.message);
     }
   }
 
   @Delete('/:index') // index 위치의 유저 삭제하기
   deleteUserByIndex(@Param('index') index: number, @Res() res: Response) {
     try {
-      this.appService.deleteUser(index);
-      res.status(200).send('deleted successfully'); //deleted successfully
+      const deletedUser = this.appService.deleteUser(index);
+      res.status(200).send(`${deletedUser} is deleted successfully`); //deleted successfully
     } catch (error) {
-      res.status(400).send(error.messages);
+      console.error('ERROR : ', error.message);
+      res.status(error.status).json(error.message);
     }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 
 @Injectable()
 export class AppService {
@@ -13,22 +18,27 @@ export class AppService {
 
   getUsernameByIndex(index: number) {
     if (index < 0 || index >= this.usernames.length) {
-      throw Error('Index out of range');
+      throw new NotFoundException('Index out of range');
     }
     return this.usernames[index]; // 인덱스 받아서 범위 안에 있으면 해당 유저네임 출력.
   }
 
   createUser(index: number, username: string) {
     if (username.length <= 0) {
-      throw Error('Invalid Username');
+      throw new BadRequestException('Invalid Username');
     }
     this.usernames.splice(index, 0, username);
+    const userInfo = {
+      createdUser: username,
+      createdIn: index,
+    };
+    return userInfo;
   }
 
   deleteUser(index: number) {
     if (index < 0 || index >= this.usernames.length) {
-      throw Error('Index out of range');
+      throw new NotFoundException('Index out of range');
     }
-    this.usernames.splice(index, 1);
+    return this.usernames.splice(index, 1);
   }
 }
