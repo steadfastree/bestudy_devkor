2f118c9f3c6e6916e0efec53966fb980f6c97a3e
Jeonghwan Park <pjhisgod@naver.com>
2023-11-25 02:01:58 +0900

    feat : swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('DevKor BEstudy')
+    .setDescription('Swagger Example of DevKor BEstudy')
+    .setVersion('1.0')
+    .build();
+
+  const docs = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, docs);
+
   await app.listen(3000);
 }
 bootstrap();
